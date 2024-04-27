4412055a6a70e9db5f7efa4d87203a6ac0a692e4
tylerwebster <webstertyler81@yahoo.com>
2024-04-27 17:02:00 -0400

    making progresss on manager

--- manager.js
+++ manager.js
@@ -1,9 +1,9 @@
 const Employee = require("./employee")
 
 class Manager extends Employee {
-    constructor(name, salary, title, manager, employees = []) {
+    constructor(name, salary, title, manager = null, employees) {
         super(name, salary, title, manager);
-        this.employees = employees;
+        this.employees = [];
         // if (this.manager === undefined) {
         //     this.manager = null
         // } else {
@@ -11,18 +11,36 @@ class Manager extends Employee {
         // }
     }
     addEmployee(employee) {
-     employee.employees.push(employee)
-                console.log(employee)
+        this.employees.push(...[employee]);
+        employee.manager = this;
+        console.log(employee)
 
     }
 }
-const splinter = new Manager('Splinter', 100000, 'Sensei');
-console.log(splinter);
 
-    splinter.addEmployee(splinter)
+const splinter = new Manager('Splinter', 100000, 'Sensai');
+console.log('Before: ', splinter);
+
+const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
+const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
+const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
+const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+
+splinter.addEmployee(leo);
+splinter.addEmployee(mikey);
+splinter.addEmployee(donnie);
+splinter.addEmployee(raph);
+
+console.log('After: ', splinter);
+
+splinter.addEmployee(splinter)
+// splinter.addEmployee(tank)
+
 
 try {
     module.exports = Manager
+
+
 } catch {
     module.exports = null;
 }
